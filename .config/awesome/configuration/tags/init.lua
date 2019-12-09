8d1f9a68d1075f23a96277b2d61c2448966d89fa
cp_shen <cp_shen@outlook.com>
2019-12-09 14:47:02 +0800

    updated

--- .config/awesome/configuration/tags/init.lua
+++ .config/awesome/configuration/tags/init.lua
@@ -6,7 +6,7 @@ local tags = {
   {
     icon = icons.chrome,
     type = 'chrome',
-    defaultApp = 'google-chrome-beta',
+    defaultApp = 'google-chrome-stable',
     screen = 1
   },
   {
--- .config/awesome/layout/init.lua
+++ .config/awesome/layout/init.lua
@@ -11,8 +11,10 @@ awful.screen.connect_for_each_screen(
       -- Create the Top bar
       s.top_panel = top_panel(s, true)
     else
+      -- Create the left_panel
+      s.left_panel = left_panel(s)
       -- Create the Top bar
-      s.top_panel = top_panel(s, false)
+      s.top_panel = top_panel(s, true)
     end
   end
 )
