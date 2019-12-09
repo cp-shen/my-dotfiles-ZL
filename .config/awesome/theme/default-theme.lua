1e61bd8cb9ef6037c0ce01f04cdec220fdc4c183
cp_shen <cp_shen@outlook.com>
2019-12-09 16:01:11 +0800

    updated

--- .config/awesome/theme/default-theme.lua
+++ .config/awesome/theme/default-theme.lua
@@ -68,7 +68,11 @@ local awesome_overrides =
   -- Taglist
 
   theme.taglist_bg_empty = theme.background.hue_800
-  theme.taglist_bg_occupied = theme.background.hue_800
+  theme.taglist_bg_occupied =
+    'linear:0,0:' ..
+            dpi(48) ..
+            ',0:0,' .. '#c4755c' .. ':0.08,' .. '#c4755c' .. ':0.08,'
+            .. theme.background.hue_800 .. ':1,' .. theme.background.hue_800
   theme.taglist_bg_urgent =
     'linear:0,0:' ..
     dpi(48) ..
