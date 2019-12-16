9105a7c6806d45ee4687110599344e31a00a9b6a
cp_shen <cp_shen@outlook.com>
2019-12-16 17:55:45 +0800

    updated

--- .emacs.d/modules/module-keys.el
+++ .emacs.d/modules/module-keys.el
@@ -1,7 +1,6 @@
 ;;; module-keys.el ---                                      -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2019  cp_shen
-
 ;; Author: cp_shen <cp_shen@cpshen-Lenovo-ideapad-700-15ISK>
 ;; Keywords:
 
@@ -26,6 +25,7 @@
 
 (require 'module-codecompletion)
 (require 'module-commandcompletion)
+(require 'module-lsp)
 
 (defun switch-to-last-buffer ()
   (interactive)
@@ -50,6 +50,13 @@
 (define-key company-active-map (kbd "C-p") #'company-select-previous)
 (define-key company-search-map (kbd "C-n") #'company-select-next)
 (define-key company-search-map (kbd "C-p") #'company-select-previous)
+
+(define-key lsp-mode-map (kbd "C-l C-b") #'lsp-find-definition)
+(define-key lsp-mode-map (kbd "C-l C-u") #'lsp-find-references)
+(define-key lsp-mode-map (kbd "C-l C-f") #'lsp-format-buffer)
+(define-key lsp-mode-map (kbd "C-l C-r") #'lsp-rename)
+
+(add-hook 'lsp-mode 'lsp-keymap-mode)
 
 (provide 'module-keys)
 ;;; module-keys.el ends here
