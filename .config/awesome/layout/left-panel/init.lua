a3e4a9b1e5acb53c0646fc91188a04e9068470ba
cp_shen <cp_shen@outlook.com>
2019-12-19 10:46:15 +0800

    updated

--- .config/awesome/layout/left-panel/init.lua
+++ .config/awesome/layout/left-panel/init.lua
@@ -6,118 +6,95 @@ local dpi = require('beautiful').xresources.apply_dpi
 local left_bar_width = require('configuration.size').left_bar_width
 
 local left_panel = function(screen)
-  local action_bar_width = dpi(left_bar_width)
-  local panel_content_width = dpi(400)
+    local action_bar_width = dpi(left_bar_width)
+    local panel_content_width = dpi(400)
 
-  local panel =
-    wibox {
-    screen = screen,
-    width = action_bar_width,
-    height = screen.geometry.height,
-    x = screen.geometry.x,
-    y = screen.geometry.y,
-    ontop = true,
-    bg = beautiful.background.hue_800,
-    fg = beautiful.fg_normal
-  }
-
-  panel.opened = false
-
-  panel:struts(
-    {
-      left = action_bar_width
-    }
-  )
-
-  local backdrop =
-    wibox {
-    ontop = true,
-    screen = screen,
-    bg = '#00000000',
-    type = 'dock',
-    x = screen.geometry.x,
-    y = screen.geometry.y,
-    width = screen.geometry.width,
-    height = screen.geometry.height
-  }
-
-  function panel:run_rofi(rofi_action)
-    local action_map = {
-      window = apps.default.rofi_window,
-      drun = apps.default.rofi,
+    local panel = wibox{
+        screen = screen,
+        width = action_bar_width,
+        height = screen.geometry.height,
+        x = screen.geometry.x,
+        y = screen.geometry.y,
+        ontop = true,
+        bg = beautiful.background.hue_800,
+        fg = beautiful.fg_normal
     }
 
-    local action = rofi_action and action_map[rofi_action]
+    panel.opened = false
 
-    if action then
-      _G.awesome.spawn(
-        action,
-        false,
-        false,
-        false,
-        false,
-        function() panel:toggle() end
-      )
-    end
-  end
+    panel:struts({left = action_bar_width})
 
-  local openPanel = function(rofi_action)
-    panel.width = action_bar_width + panel_content_width
-    backdrop.visible = true
-    panel.visible = false
-    panel.visible = true
-    panel:get_children_by_id('panel_content')[1].visible = true
-    if rofi_action then
-      panel:run_rofi(rofi_action)
-    end
-    panel:emit_signal('opened')
-  end
+    local backdrop = wibox{
+        ontop = true,
+        screen = screen,
+        bg = '#00000000',
+        type = 'dock',
+        x = screen.geometry.x,
+        y = screen.geometry.y,
+        width = screen.geometry.width,
+        height = screen.geometry.height
+    }
 
-  local closePanel = function()
-    panel.width = action_bar_width
-    panel:get_children_by_id('panel_content')[1].visible = false
-    backdrop.visible = false
-    panel:emit_signal('closed')
-  end
+    function panel:run_rofi(rofi_action)
+        local action_map = {
+            window = apps.default.rofi_window,
+            drun = apps.default.rofi
+        }
 
-  function panel:toggle(rofi_action)
-    self.opened = not self.opened
-    if self.opened then
-      openPanel(rofi_action)
-    else
-      closePanel()
-    end
-  end
+        local action = rofi_action and action_map[rofi_action]
 
-  backdrop:buttons(
-    awful.util.table.join(
-      awful.button(
-        {},
-        1,
-        function()
-          panel:toggle()
+        if action then
+            _G.awesome.spawn(action, false, false, false, false,
+                             function() panel:toggle() end)
         end
-      )
-    )
-  )
+    end
 
-  panel:setup {
-    layout = wibox.layout.align.horizontal,
-    nil,
-    {
-      id = 'panel_content',
-      bg = beautiful.background.hue_900,
-      widget = wibox.container.background,
-      visible = false,
-      forced_width = panel_content_width,
-      {
-        require('layout.left-panel.dashboard')(screen, panel),
-        layout = wibox.layout.stack
-      }
-    },
-    require('layout.left-panel.action-bar')(screen, panel, action_bar_width)
-  }
-  return panel
+    local openPanel = function(rofi_action)
+        panel.width = action_bar_width + panel_content_width
+        backdrop.visible = true
+        panel.visible = false
+        panel.visible = true
+        panel:get_children_by_id('panel_content')[1].visible = true
+        if rofi_action then panel:run_rofi(rofi_action) end
+        panel:emit_signal('opened')
+    end
+
+    local closePanel = function()
+        panel.width = action_bar_width
+        panel:get_children_by_id('panel_content')[1].visible = false
+        backdrop.visible = false
+        panel:emit_signal('closed')
+    end
+
+    function panel:toggle(rofi_action)
+        self.opened = not self.opened
+        if self.opened then
+            openPanel(rofi_action)
+        else
+            closePanel()
+        end
+    end
+
+    backdrop:buttons(awful.util.table.join(
+                         awful.button({}, 1, function() panel:toggle() end)))
+
+    panel:setup{
+        layout = wibox.layout.align.horizontal,
+        nil,
+        {
+            id = 'panel_content',
+            bg = beautiful.background.hue_900,
+            widget = wibox.container.background,
+            visible = false,
+            forced_width = panel_content_width,
+            {
+                require('layout.left-panel.dashboard')(screen, panel),
+                layout = wibox.layout.stack
+            }
+        },
+        require('layout.left-panel.action-bar')(screen, panel, action_bar_width)
+    }
+    return panel
 end
 
 return left_panel
