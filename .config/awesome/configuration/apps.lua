82c07cfaa5b66b362a629b1d5a2c5fef00359669
cp_shen <cp_shen@outlook.com>
2020-02-10 21:52:59 +0800

    updated

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -26,7 +26,7 @@ local apps = {
         'xfce4-power-manager', -- Power manager
         'nm-applet', -- network manager
         -- '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)' -- credential manager
-        '~/Programs/clash/clash-linux-amd64-v0.15.0 -d ~/Programs/clash/'
+        '~/ProgramFiles/clash/clash -d ~/ProgramFiles/clash/'
     },
 
     -- used to define rules
